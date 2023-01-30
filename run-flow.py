f2dd77376b45cb5245407b030475cd3db6c52968
Radical Labs <marius@everbum.com>
2023-01-30 19:48:07 +0100

    small changes

--- run-flow.py
+++ run-flow.py
@@ -1,13 +1,31 @@
 import platform
 import os
+import sys
+import importlib
 
-system = platform.system()
+# system = platform.system()
 
-if system == "Windows":
-  print("RUN FLOW - Running on Windows")
-  # Windows specific commands
-  os.system("dir")
-elif system == "Linux":
-  print("RUN FLOW - Running on Unix-based system")
-  # Unix-based system specific commands
-  os.system("ls")
+# if system == "Windows":
+#   print("RUN FLOW - Running on Windows")
+#   # Windows specific commands
+#   os.system("dir")
+# elif system == "Linux":
+#   print("RUN FLOW - Running on Unix-based system")
+#   # Unix-based system specific commands
+#   os.system("ls")
+
+
+# if len(sys.argv) != 3:
+#     print("Usage: run-flow.py <filename.taskname> <user>")
+#     sys.exit(1)
+
+taskSource, taskName = sys.argv[1].split('.')
+userId = sys.argv[2]
+
+print("Task:", taskSource, taskName)
+print("User:", userId)
+
+module = importlib.import_module(f'tasks.{taskSource}')
+task_class = getattr(module, taskName)
+task = task_class()
+task.run()
