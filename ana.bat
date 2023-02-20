1b5f56c61225651cab491c5b5310cf7668c606f4
Marius <marius@everbum.com>
2023-02-20 09:49:12 +0100

    twiking console for VSC

---
+++ ana.bat
@@ -0,0 +1,8 @@
+@echo off
+@REM set "new_path=c:\PROGRA~1\Anaconda3;c:\PROGRA~1\Anaconda3\Library\mingw-w64\bin;c:\PROGRA~1\Anaconda3\Library\usr\bin;c:\PROGRA~1\Anaconda3\Library\bin;c:\PROGRA~1\Anaconda3\Scripts;c:\PROGRA~1\Anaconda3\bin;c:\PROGRA~1\Anaconda3\condabin"
+
+@REM set "existing_path=%PATH%"
+@REM set "updated_path=%existing_path%;%new_path%"
+
+set PATH=c:\PROGRA~1\Anaconda3;c:\PROGRA~1\Anaconda3\Library\mingw-w64\bin;c:\PROGRA~1\Anaconda3\Library\usr\bin;c:\PROGRA~1\Anaconda3\Library\bin;c:\PROGRA~1\Anaconda3\Scripts;c:\PROGRA~1\Anaconda3\bin;c:\PROGRA~1\Anaconda3\condabin;%PATH%
+echo PATH system variable has been updated.
